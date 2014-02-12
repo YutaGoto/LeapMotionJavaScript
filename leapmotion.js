1e78810f7ff1311bae8022fa9f918ef7603c6c21
YutaGoto <you.goto.510@gmail.com>
2014-02-12 11:02:32 +0900

    Edit js code

--- leapmotion.js
+++ leapmotion.js
@@ -1,15 +1,5 @@
-	// Pulling the Canvas DOM element into Javascript
-var canvas = document.getElementById('canvas');
-
-// Setting up the context we want to use
-// var c =  canvas.getContext('2d');
-
-// Prepping our unicorn
-var img = document.createElement('img');
-img.src = "http://i.imgur.com/f68W5EC.png";
-
 // Creating our leap controller
-var controller = new Leap.Controller();
+var controller = new Leap.Controller({ enableGestures: true });
 
 // Proving that the websocket is open
 controller.on( 'connect' , function(){
@@ -35,9 +25,16 @@ controller.on( 'deviceDisconnected' , function() {
 // When the controller is ready, spawn the unicorn!
 controller.on( 'ready' , function(){
 
-  // c.drawImage( img , 0 , 0 );
+  console.log('aaa');
  
 });
+
+controller.on('frame', function(data) {
+  // your code here
+  frame = data;
+  // console.log('abc');
+});
+
   
 controller.connect();
 
